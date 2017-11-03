67159c6f15e62ef39ef3a3b596b1ef1da2a1452b
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2017-11-02 21:49:24 -0700

    Add variables and append list

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,28 @@
 //Business Logic Here
 
-function Account() {
+function Account(name, original, deposit, withdrawal, balance) {
   this.name = name;
+  this.original = original;
   this.deposit = deposit;
-  this.withdrawl = withdrawal;
+  this.withdrawal = withdrawal;
   this.balance = balance;
 }
 
-Account.prototype.newNamehere()
+// Account.prototype.newNamehere()
 
 // Interface Logic Here
 
 $(document).ready(function() {
-  $("#").submit(function(event) {
-  event.preventDefault();
+  $("#start").submit(function(event) {
+    event.preventDefault();
 
+    var newAccount = $("#new-account-name").val();
+    var originalDeposit = parseInt($("#original-deposit").val());
 
+    var account = new Account(newAccount, originalDeposit)
+
+    $("#show-name").append("<li>" + newAccount + "</span></li>");
+    $("#show-balance").append("<li>" + originalDeposit + "</span></li>");
 
   });
 });
